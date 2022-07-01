e1ce6bccfdab0cf38f1a8f76cfeb29629e69a5f2
IntoRu <crospit@gmail.com>
2022-07-01 05:48:33 +0300

    create new app

---
+++ sript.js
@@ -0,0 +1,122 @@
+const searchInput = document.querySelector('input')
+const list = document.getElementById('list')
+const btn = document.querySelector('button')
+const div = document.querySelector('div')
+const img = document.querySelector('.nameImg')
+const infoDiv = document.querySelector('.infoDiv')
+// -----------------------------------------------------------
+
+// --- function Live Search ------
+
+function liveSearch(data) {
+
+
+    function setList(group) {
+        clearList()
+        for (const person of group) {
+            const item = document.createElement('li')
+            item.classList.add('item')
+            const text = document.createTextNode(person.name)
+            item.appendChild(text)
+            list.appendChild(item)
+        }
+        if (group.length === 0) {
+            setNoResults()
+        }
+
+        touchSpisok()
+    }
+
+    function clearList() {
+        while (list.firstChild) {
+            list.removeChild(list.firstChild)
+        }
+    }
+
+    function setNoResults() {
+        const item = document.createElement('li')
+        item.classList.add('item')
+        const text = document.createTextNode('No results found')
+        item.appendChild(text)
+        list.appendChild(item)
+    }
+
+    function getRelevancy(value, searchTerm) {
+        if (value === searchTerm) {
+            return 2
+        }
+        else if (value.startsWith(searchTerm)) {
+            return 1
+        }
+        else if (value.includes(searchTerm)) {
+            return 0
+        }
+        else {
+            return -1
+        }
+    }
+
+    searchInput.addEventListener('input', (event) => {
+        img.src = "https://flaglab.ru/uploads/5ac891fd437a1doc1301297432/5ac898cc184e2.svg"
+        infoDiv.innerHTML = ''
+        let value = event.target.value
+        if (value && value.trim().length > 0) {
+            value = value.trim().toUpperCase()
+            setList(data.filter(person => {
+                return person.name.toUpperCase().includes(value)
+            }).sort((personA, personB) => {
+                return getRelevancy(personB.name, value) - getRelevancy(personA.name, value)
+            }))
+        }
+        else {
+            clearList()
+        }
+    })
+
+    function touchSpisok() {
+        let spisok = Array.from(document.querySelectorAll('.item'))
+        spisok.forEach(el => {
+            el.addEventListener('click', (e) => {
+                // console.log(e.target.textContent)
+                searchInput.value = e.target.textContent
+                clearList()
+
+            })
+        })
+    }
+}
+
+// --------------------------------------------------------------------------------------------
+
+// --- Async function sever request ----------------------
+
+async function getCountry() {
+    const response = await fetch('https://api.sampleapis.com/countries/countries')
+    const data = await response.json()
+
+    // call function live searsh
+    liveSearch(data)
+
+    btn.addEventListener('click', () => {
+        data.forEach(el => {
+            // console.log(el.flag)
+            if (searchInput.value === el.name) {
+                img.src = el.media.flag
+
+                let info = `
+                <p>Abbreviation - ${el.abbreviation}<p>
+                <p>Capital - ${el.capital}<p>
+                <p>Currency - ${el.currency}<p>
+                <p>Phone - ${el.phone}<p>
+                <p>Population - ${el.population}<p>
+            `
+                infoDiv.innerHTML = info
+            }
+        })
+        searchInput.value = ''
+    })
+
+}
+
+// call function server request
+getCountry()
